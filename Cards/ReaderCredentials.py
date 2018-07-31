894679ac6c5dfaa02645d918d5b32d7267ced856
CardsApp <info@cardsapp.com>
2018-07-31 09:04:50 +0300

    Remove leftover folder (Cards)

--- Cards/ReaderCredentials.py
+++
@@ -1,4 +0,0 @@
-class ReaderCredentials:
-    """Reader Credentials Class"""
-    def __init__(self, api_key):
-        self.api_key = api_key
--- Cards/ReaderSettings.py
+++
@@ -1,4 +0,0 @@
-class ReaderSettings:
-    """Reader Settings Class"""
-    def __init__(self, device_name):
-        self.device_name = device_name
